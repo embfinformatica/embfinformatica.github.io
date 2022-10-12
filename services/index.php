a0a0d6b0053b4fccbb3c6bceda25cda60a0ec436
EMBF Softwares e Investigação <consultoria@eduardoprogramador.com>
2022-10-12 15:48:21 -0300

    Update index.php

--- services/index.php
+++ services/index.php
@@ -1,22 +1,7 @@
-<!DOCTYPE html>
-<head>    
-    <html lang="pt-BR">
-    <meta charset="UTF-8">
-    <title>Qual é o seu signo?</title>
-    <link rel="stylesheet" type="text/css" href="signo.css">
-    <script type="text/javascript" src="signo.js"></script>
-</head>
-<header>
-    <div class="information">
-        <label for="date_i">Informe sua data de Nascimento:</label>
-        <input type="date" id="date"><br>
-        <button id="button">Confirmar</button>
-    </div>
-    
-</header>
-<body>
-    <script>
-        document.onload = main();
-    </script>
-</body>
-</html>
+<?php
+header('Access-Control-Allow-Origin: *');
+$fd = fopen('signo.xml','r');
+$xml = fread($fd, filesize('signo.xml'));
+echo($xml);
+fclose($fd);
+?>
